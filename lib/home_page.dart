69d9f95cff6e6c4a159beeb777ad769dc1d02809
Motiur_dev <motiur.togumogu@gmail.com>
2024-08-09 18:17:44 +0600

    animation login page update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,26 +26,30 @@ class _HomePageState extends State<HomePage> {
                     top: -40,
                     height: 400,
                     width: width,
-                    child: FadeInUp(duration: const Duration(seconds: 1), child: Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/background.png'),
-                              fit: BoxFit.fill
-                          )
-                      ),
-                    )),
+                    child: FadeInUp(
+                        duration: const Duration(seconds: 1),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/background.png'),
+                                  fit: BoxFit.fill)),
+                        )),
                   ),
                   Positioned(
                     height: 400,
-                    width: width+20,
-                    child: FadeInUp(duration: const Duration(milliseconds: 1000), child: Container(
-                      decoration: const BoxDecoration(
+                    width: width + 20,
+                    child: FadeInUp(
+                      duration: const Duration(milliseconds: 1000),
+                      child: Container(
+                        decoration: const BoxDecoration(
                           image: DecorationImage(
-                              image: AssetImage('assets/images/background-2.png'),
-                              fit: BoxFit.fill,
+                            image: AssetImage('assets/images/background-2.png'),
+                            fit: BoxFit.fill,
                           ),
+                        ),
                       ),
-                    ),),
+                    ),
                   ),
                 ],
               ),
@@ -55,68 +59,112 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeInUp(duration: const Duration(milliseconds: 1500), child: const Text("Login", style: TextStyle(color: Color.fromRGBO(49, 39, 79, 1), fontWeight: FontWeight.bold, fontSize: 30),)),
-                  const SizedBox(height: 30,),
-                  FadeInUp(duration: const Duration(milliseconds: 1700), child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        border: Border.all(color: const Color.fromRGBO(196, 135, 198, .3)),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color.fromRGBO(196, 135, 198, .3),
-                            blurRadius: 20,
-                            offset: Offset(0, 10),
-                          )
-                        ]
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: const BoxDecoration(
-                              border: Border(bottom: BorderSide(
-                                  color: Color.fromRGBO(196, 135, 198, .3)
-                              ))
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Username",
-                                hintStyle: TextStyle(color: Colors.grey.shade700)
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 1500),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Color.fromRGBO(49, 39, 79, 1),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 1700),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            border: Border.all(
+                              width: 1.5,
+                                color: const Color.fromRGBO(196, 135, 198, .3)),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(196, 135, 198, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              ),
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 1.5,
+                                      color: Color.fromRGBO(196, 135, 198, .3)),
+                                ),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Username",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
                             ),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 1700),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    color: Color.fromRGBO(196, 135, 198, 1)),
+                              )))),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 1900),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        color: const Color.fromRGBO(49, 39, 79, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        height: 50,
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.grey.shade700)
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  )),
-                  const SizedBox(height: 20,),
-                  FadeInUp(duration: const Duration(milliseconds: 1700), child: Center(child: TextButton(onPressed: () {}, child: const Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(196, 135, 198, 1)),)))),
-                  const SizedBox(height: 30,),
-                  FadeInUp(duration: const Duration(milliseconds: 1900), child: MaterialButton(
-                    onPressed: () {},
-                    color: const Color.fromRGBO(49, 39, 79, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    height: 50,
-                    child: const Center(
-                      child: Text("Login", style: TextStyle(color: Colors.white),),
-                    ),
-                  )),
-                  const SizedBox(height: 30,),
-                  FadeInUp(duration: const Duration(milliseconds: 2000), child: Center(child: TextButton(onPressed: () {}, child: const Text("Create Account", style: TextStyle(color: Color.fromRGBO(49, 39, 79, .6)),)))),
+                      )),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 2000),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Create Account",
+                                style: TextStyle(
+                                    color: Color.fromRGBO(49, 39, 79, .6)),
+                              )))),
                 ],
               ),
             )
